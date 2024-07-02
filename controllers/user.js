fa3d2822e10fd37e155572050f157302e0427498
TheRockMilad <therock.milad2@outlook.com>
2024-07-02 19:30:46 +0330

    add joi middleware to signup and controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,27 @@
 const { validationResult } = require("express-validator");
+const Joi = require("joi");
+const { userValidatorSchema } = require("../validators/user.validator");
 
 exports.siqnUp = async (req, res) => {
-//     const result = validationResult(req);
-//     if (!result.isEmpty()) {
-//        const message = {}
-//        const errors = result.array()
-//        for (const error of errors) {
-//         message[error.path] = error.msg;
-//         }
-//       //   const obj = {};
-//       // result.errors.forEach((error) => {
-//       //   obj[error.path] = error.msg;
-//       // });
-//       return res.json(message)
-  
-//       // return res.json(message);
-//     }
-  
-//     return res.json({ msg: "User register successfully :))" });
-  }
+  //express-validator
+  // باید بره توی میدلور
+  //     const result = validationResult(req);
+  //     if (!result.isEmpty()) {
+  //        const message = {}
+  //        const errors = result.array()
+  //        for (const error of errors) {
+  //         message[error.path] = error.msg;
+  //         }
+  //       //   const obj = {};
+  //       // result.errors.forEach((error) => {
+  //       //   obj[error.path] = error.msg;
+  //       // });
+  //       return res.json(message)
+  //       // return res.json(message);
+  //     }
+  //     return res.json({ msg: "User register successfully :))" });
+  //Joi validatror :
+
+    res.json("register user successfully");
+  };
+
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { userValidator } = require("../validators/user.validator");
-const userController = require("./../controllers/user")
+const userController = require("./../controllers/user");
+const {userValidatorSchema} = require('./../validators/user.validator');
+const validate = require("../middlewares/joi.validator");
 
 const router = express.Router();
 
-router.post("/signup", userValidator(),userController.siqnUp);
+// router.post("/signup", userValidator(),userController.siqnUp); //express-validator
+router.post("/signup",validate(userValidatorSchema),userController.siqnUp);
 
 module.exports = router;
