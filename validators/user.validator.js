dddf68e75c03b1112a7f7859218885f9ffb1d990
TheRockMilad <therock.milad2@outlook.com>
2024-07-02 19:27:06 +0330

    Add Joi validator schema

--- validators/user.validator.js
+++ validators/user.validator.js
@@ -1,37 +1,60 @@
-const { body } = require("express-validator");
+// const { body } = require("express-validator");
 
-const userValidator = () => {
-  return [
-    body("firstname")
-      .isString()
-      .withMessage("لطفا نام را به شکل متن وارد کنید")
-      .isLength({ min: 3, max: 15 })
-      .withMessage("نام شما باید بین 3 تا 15 کاراکتر باشه"),
+const Joi = require("joi");
 
-    body("lastname")
-      .isString()
-      .withMessage("لطفا نام خانوادگی را به شکل متن وارد کنید")
-      .isLength({ min: 3, max: 15 })
-      .withMessage("نام خانوادگی باید بین 3 تا 15 کاراکتر باشه"),
+// express-validator :
 
-    body("username")
-      .isString()
-      .withMessage("لطفا نام کاربری را به شکل متن وارد کنید")
-      .isLength({ min: 8, max: 24 })
-      .withMessage("نام کاربری باید بین 8 تا 24 کاراکتر باشه"),
+// const userValidator = () => {
+//   return [
+//     body("firstname")
+//       .isString()
+//       .withMessage("لطفا نام را به شکل متن وارد کنید")
+//       .isLength({ min: 3, max: 15 })
+//       .withMessage("نام شما باید بین 3 تا 15 کاراکتر باشه"),
 
-    body("email").isEmail().withMessage("ایمیل وارد شده معتبر نمی‌باشد"),
+//     body("lastname")
+//       .isString()
+//       .withMessage("لطفا نام خانوادگی را به شکل متن وارد کنید")
+//       .isLength({ min: 3, max: 15 })
+//       .withMessage("نام خانوادگی باید بین 3 تا 15 کاراکتر باشه"),
 
-    body("phone")
-      .isMobilePhone(["fa-IR"])
-      .withMessage("شماره تماس وارد شده معتبر نمی‌باشد")
-      .notEmpty()
-      .withMessage("نمیتواند خالی باشه "),
+//     body("username")
+//       .isString()
+//       .withMessage("لطفا نام کاربری را به شکل متن وارد کنید")
+//       .isLength({ min: 8, max: 24 })
+//       .withMessage("نام کاربری باید بین 8 تا 24 کاراکتر باشه"),
 
-    body("password")
-      .isLength({ min: 8, max: 24 })
-      .withMessage("پسورد باید بین 8 تا 24 کاراکتر باشه"),
-  ];
-};
+//     body("email").isEmail().withMessage("ایمیل وارد شده معتبر نمی‌باشد"),
 
-module.exports = { userValidator };
+//     body("phone")
+//       .isMobilePhone(["fa-IR"])
+//       .withMessage("شماره تماس وارد شده معتبر نمی‌باشد")
+//       .notEmpty()
+//       .withMessage("نمیتواند خالی باشه "),
+
+//     body("password")
+//       .isLength({ min: 8, max: 24 })
+//       .withMessage("پسورد باید بین 8 تا 24 کاراکتر باشه"),
+//   ];
+// };
+
+//Joi validator
+const userValidatorSchema = Joi.object({
+  firstname: Joi.string().lowercase().min(3).max(15).required(), // AMIn -> amin
+  lastname: Joi.string().uppercase().min(3).max(15).required(),
+  username: Joi.string().min(8).max(24).required(),
+  signupMethod: Joi.string().required().valid("email", "phone"),
+  email: Joi.string()
+    .email()
+    .min(10)
+    .max(40)
+    .when("signupMethod", { is: "email", then: Joi.required() }),
+  phone: Joi.string()
+    .pattern(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+    .when("signupMethod", { is: "phone", then: Joi.required() }),
+  password: Joi.string().min(8).max(24).required(),
+  confirmPassword: Joi.ref("password"),
+});
+
+// module.exports = { userValidators };
+module.exports = { userValidatorSchema };
