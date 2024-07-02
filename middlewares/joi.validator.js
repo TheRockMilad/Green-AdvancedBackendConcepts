5a6011ae3bf129ddc0116b054f0b8af14623c00d
TheRockMilad <therock.milad2@outlook.com>
2024-07-02 19:47:32 +0330

    add isJoi

--- middlewares/joi.validator.js
+++ middlewares/joi.validator.js
@@ -1,16 +1,21 @@
 const validate = (schema) => {
   return async (req, res, next) => {
     try {
-      await schema.validateAsync(req.body, { abortEarly: false });
+      await schema.validateAsync(req.body, { abortEarly: false }); //400
       next();
     } catch (err) {
-      const errObject = {};
-      
-      err.details.forEach((errorDetail) => {
-        errObject[errorDetail.context.key] = errorDetail.message.replace(/"/g, "");
-      });
+      if (err.isJoi) {
+        const errObject = {};
+        err.details.forEach((errorDetail) => {
+          errObject[errorDetail.context.key] = errorDetail.message.replace(
+            /"/g,
+            ""
+          );
+        });
+        return res.status(400).json(errObject); //400
+      }
 
-      return res.status(400).json(errObject);
+      return res.status(500).json({ message: err.message }); //500
     }
   };
 };
